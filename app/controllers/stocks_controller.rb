c5db5ebd1eb9774be8d607ab6a280fd2abb83853
paradon61143227 <mouth_hd@hotmail.co.th>
2021-11-18 14:59:46 +0700

    OK

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,39 @@
 class StocksController < ApplicationController
   def index
+    @stock = Stock.all
+  end
+
+  def show
+    @stock = Stock.find(params[:id])
+  end
+
+  def new
+    @stock = Stock.new
+  end
+
+  def create
+    @stock = Stock.new(stock_params)
+    @stock.save if redirect_to @stock
+  end
+
+  def edit
+    @stock = Stock.find(params[:id])
+  end
+
+  def update
+    @stock = Stock.find(params[:id])
+    redirect_to @stock if  @stock.update(stock_params)
+  end
+
+  def destroy
+    @stock = Stock.find(params[:id])
+    @stock.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:name, :qty)
   end
 end
